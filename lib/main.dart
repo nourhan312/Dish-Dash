b75abec2d096810168f55addbb69a0451c3ee69c
nourhan312 <nourhanaymanghonim@gmail.com>
2024-10-31 05:56:35 +0300

    prepare project

---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:another_flutter_splash_screen/another_flutter_splash_screen.dart';
+import 'package:dish_dash/home.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_native_splash/flutter_native_splash.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: const Home(),
+    );
+  }
+}
